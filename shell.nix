08a4f0af3f1cbd1d8d387d88142da6de3921081a
Victor Engmark <vengmark@linz.govt.nz>
2023-08-25 10:15:56 +1200

    feat: Include yarn packages in Nix shell

--- shell.nix
+++ shell.nix
@@ -1,11 +1,16 @@
 let
   sources = import ./nix/sources.nix;
   pkgs = import sources.nixpkgs {};
+  yarn = pkgs.yarn;
+  yarnPackages = pkgs.yarn2nix-moretea.mkYarnWorkspace {
+    inherit yarn;
+    src = ./.;
+  };
 in
   pkgs.mkShell {
     packages = [
       pkgs.bashInteractive
       pkgs.nodejs
-      pkgs.yarn
-    ];
+      yarn
+    ] ++ (builtins.attrValues yarnPackages);
   }
