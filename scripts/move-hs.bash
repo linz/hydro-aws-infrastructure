b3bbb884034d6d1444441be7b21a589ea2502eb6
Victor Engmark <vengmark@linz.govt.nz>
2023-10-02 14:59:50 +1300

    chore: Comment out some finished commands

--- scripts/move-hs.bash
+++ scripts/move-hs.bash
@@ -10,7 +10,7 @@ move_command=(echo aws s3 mv --dryrun --recursive)
 bucket_root='s3://hydro-data-bucket-418528898914'
 authoritative_surveys="${bucket_root}/Authoritative_Surveys"
 
-echo 'Move everything from HSXX-XX to Authoritative_Surveys'
+#echo 'Move everything from HSXX-XX to Authoritative_Surveys'
 #
 # This replaces the first part of the prefixes with a fixed string, so we have to use `scripts/dupes.bash hsxx-xx.log`
 # to check for duplicates before running for real.
@@ -18,10 +18,11 @@ echo 'Move everything from HSXX-XX to Authoritative_Surveys'
 # Found all prefixes by running
 # `aws s3 ls s3://hydro-data-bucket-418528898914 | rg --only-matching --pcre2 '(?<=^                           PRE )HS\d\d-\d\d.*'`
 #
+#for prefix in HS10-19 HS47-51 HS52-54 HS55-57 HS70-79; do
+#    "${move_command[@]}" "${bucket_root}/${prefix}/" "${authoritative_surveys}/"
+#done
+
 # TODO: Include HS60-69 when ready
-for prefix in HS10-19 HS47-51 HS52-54 HS55-57 HS70-79; do
-    "${move_command[@]}" "${bucket_root}/${prefix}/" "${authoritative_surveys}/"
-done
 
 echo
 echo 'Move HS71, which is highest priority'
