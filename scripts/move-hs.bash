92ea7c3ec22b9347225c5f0aa49e1ff6e100f1a3
Victor Engmark <vengmark@linz.govt.nz>
2023-10-02 15:18:30 +1300

    chore: Comment out finished command

--- scripts/move-hs.bash
+++ scripts/move-hs.bash
@@ -32,14 +32,14 @@ echo 'Move HS71, which is highest priority'
     "${bucket_root}/HS71/" \
     "${authoritative_surveys}/HS71/"
 
-echo
-echo
-pre='HS41 - Whangaroa Harbour'
-post='HS41_Whangaroa_Harbour'
-echo "Rename '${pre}' to 'Authoritative_Surveys/${post}'"
-"${move_command[@]}" \
-    "${bucket_root}/${pre}/" \
-    "${authoritative_surveys}/${post}/"
+#echo
+#echo
+#pre='HS41 - Whangaroa Harbour'
+#post='HS41_Whangaroa_Harbour'
+#echo "Rename '${pre}' to 'Authoritative_Surveys/${post}'"
+#"${move_command[@]}" \
+#    "${bucket_root}/${pre}/" \
+#    "${authoritative_surveys}/${post}/"
 
 echo
 echo
