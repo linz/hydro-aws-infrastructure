a6927e7a9aaaf92c3a6ae25fea0f6066ce389bdd
Victor Engmark <vengmark@linz.govt.nz>
2023-10-02 15:59:53 +1300

    feat: Actually run dry-run commands

--- scripts/move-hs.bash
+++ scripts/move-hs.bash
@@ -5,7 +5,7 @@ shopt -s failglob inherit_errexit
 
 # Prefix `echo` to just print the commands
 # Suffix `--dryrun` to do a dry run, not actually moving any files
-move_command=(printf '%q ' aws s3 mv --dryrun --recursive)
+move_command=(aws s3 mv --dryrun --recursive)
 
 bucket_root='s3://hydro-data-bucket-418528898914'
 authoritative_surveys="${bucket_root}/Authoritative_Surveys"
